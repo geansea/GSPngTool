76618e7c86990e8bc9fcf9d03c3eaa0fb1f919cb
geansea <geansea@126.com>
2018-03-10 12:32:36 +0800

    Add init project

---
+++ GSPngLib/GSPngLib.cpp
@@ -0,0 +1,6 @@
+#include "GSPngLib.h"
+
+
+GSPngLib::GSPngLib()
+{
+}
---
+++ GSPngLib/GSPngLib.h
@@ -0,0 +1,12 @@
+#ifndef GSPNGLIB_H
+#define GSPNGLIB_H
+
+
+class GSPngLib
+{
+
+public:
+    GSPngLib();
+};
+
+#endif // GSPNGLIB_H
